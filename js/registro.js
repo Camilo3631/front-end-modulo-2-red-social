67ab5d56650b5884c4cb73f0c984eb6e9f9f3a46
crislozanor05 <cristina.lozanor05@alumnostrazos.net>
2026-05-29 17:07:19 +0200

    inicio funcion boton registro

---
+++ js/registro.js
@@ -0,0 +1,24 @@
+function registrar() {
+  const username = document.getElementById("reg-username").value;
+  const email = document.getElementById("reg-email").value;
+  const password = document.getElementById("reg-contrasena").value;
+
+  fetch(`http://localhost:3000/api/clientes/registrar-cliente`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ usuario: username, email: email, contrasena: password }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.data.insertedId) {
+        document.getElementById("register-message").innerText =
+          "usuario registrado";
+        setTimeout(() => {
+          document.getElementById("register-message").innerText = "";
+        }, 2000);
+      }
+    });
+}
