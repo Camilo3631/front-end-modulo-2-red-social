72ce9c1816c73021bd7748c81c5edc39a55e8e3d
Karla <karla.perezn96@alumnostrazos.net>
2026-06-01 19:00:06 +0200

    Mostrar contacto luego del login - incompleto

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function iniciarSesion() {
-    const email = document.getElementById("login-email").value;
-    const password = document.getElementById("login-contrasena").value;
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-contrasena").value;
 
-    fetch(`http://localhost:3000/usuarios/iniciar-sesion`, {
+  fetch(`http://localhost:3000/usuarios/iniciar-sesion`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,17 +14,21 @@ function iniciarSesion() {
   })
     .then((res) => res.json())
     .then((data) => {
-    console.log(data);
+      console.log(data);
 
       if (data.estado) {
-        fetch('../html/contactos.html').then(res => res.text()).then(html => { 
-          document.getElementById('contenido').innerHTML= html})
+        fetch("../html/contactos.html")
+          .then((res) => res.text())
+          .then((html) => {
+            document.getElementById("contenido").innerHTML = html;
+          });
 
-          
+        localStorage.setItems("email", data.usuarioVerificado.email);
       } else {
-        document.getElementById('error-inicio-sesion').innerText = "Credenciales incorrectas"
+        document.getElementById("error-inicio-sesion").innerText =
+          "Credenciales incorrectas";
       }
-
-      
     });
 }
+
+function navPerfil() {}
