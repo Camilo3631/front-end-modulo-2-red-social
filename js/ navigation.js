945282fa959940e543446f2c0a0a9f9db9331177
crislozanor05 <cristina.lozanor05@alumnostrazos.net>
2026-06-02 16:19:33 +0200

    panel ajustes

---
+++ js/ navigation.js
@@ -0,0 +1,4 @@
+function toggleConfig() {
+      document.getElementById("panel-config").classList.toggle("abierto");
+      document.getElementById("perfil-contenido").classList.toggle("reducido");
+    }
