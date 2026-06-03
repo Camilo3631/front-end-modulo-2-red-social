21d78fd79f46c99f39e723ad54afe56f4d0f059d
Karla <karla.perezn96@alumnostrazos.net>
2026-06-03 18:55:50 +0200

    funcion mostrar contacto

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+const url= 'http://localhost:3000';
+
 function iniciarSesion() {
   const email = document.getElementById("login-email").value;
   const password = document.getElementById("login-contrasena").value;
 
-  fetch(`http://localhost:3000/usuarios/iniciar-sesion`, {
+  fetch(`${url}/usuarios/iniciar-sesion`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,23 +19,44 @@ function iniciarSesion() {
       console.log(data);
 
       if (data.estado) {
+        guardarUsuario(data)
+        listaContactos(data)
         
-        fetch("../html/contactos.html")
-          .then((res) => res.text())
-          .then((html) => {
-            document.getElementById("contenido").innerHTML = html;
-            console.log(data)
-          });
-
-          localStorage.setItem("id", data.usuarioVerificado._id);
-          localStorage.setItem("email", data.usuarioVerificado.email);
-          localStorage.setItem("username", data.usuarioVerificado.username);
 
       } else {
         document.getElementById("error-inicio-sesion").innerText =
           "Credenciales incorrectas";
       }
     });
+}
+
+function listaContactos(data){
+  fetch("../html/contactos.html")
+          .then((res) => res.text())
+          .then((html) => {
+            document.getElementById("contenido").innerHTML = html;
+            console.log(data)
+          });
+         let username = data.usuarioVerificado.username
+
+          fetch(`${url}/contactos/${username}`).then(res => res.json()).then(data => {
+            data.forEach(contacto => {
+              let contactoUsername = contacto.username_contacto1 === username ? contacto.username_contacto2 : contacto.username_contacto1
+            })
+            document.getElementById('lista-contactos').innerHTML += `
+            <div>
+            ${contactoUsername}
+            </div>
+            `
+          })
+}
+
+//Para personalizar pantalla de perfil/contactos con datos del usuario
+function guardarUsuario(data) {
+          localStorage.setItem("id", data.usuarioVerificado._id);
+          localStorage.setItem("email", data.usuarioVerificado.email);
+          localStorage.setItem("username", data.usuarioVerificado.username);
+
 }
 
 
