e431292a9a7af4c29a40ef4a001ffb8d32928220
crislozanor05 <cristina.lozanor05@alumnostrazos.net>
2026-06-04 11:20:00 +0200

    ajustes

--- js/ navigation.js
+++ js/ navigation.js
@@ -1,13 +1,11 @@
-
 // Panel ajustes dentro del perfil
 
 function toggleConfig() {
-      document.getElementById("panel-config").classList.toggle("abierto");
-      document.getElementById("perfil-contenido").classList.toggle("reducido");
-    }
+  document.getElementById("panel-config").classList.toggle("abierto");
+  document.getElementById("perfil-contenido").classList.toggle("reducido");
+}
 
-
-// NAVBAR    
+// NAVBAR
 function navContactos() {
   window.location.href = "contactos.html";
 }
@@ -17,21 +15,26 @@ function navPerfil() {
 }
 
 //perfil
-// ── SELECCIONAR AVATAR ────────────────────────────────────────────────────────
-function seleccionarAvatar(i) {
+// SELECCIONAR AVATAR
+function seleccionarAvatar(div) {
   // Quitar selección anterior
-  document.querySelectorAll(".avatar-opcion").forEach(a => a.classList.remove("seleccionado"));
-  i.classList.add("seleccionado");
- 
-  // Actualizar foto de perfil en pantalla
-  document.getElementById("foto-perfil").src = i.src;
+  document
+    .querySelectorAll(".avatar-opcion")
+    .forEach((a) => a.classList.remove("seleccionado"));
+  div.classList.add("seleccionado");
+
+  // Copiar el icono y el color al div de la foto de perfil
+  const iconoSeleccionado = div.querySelector("i");
+  const fotoPerfil = document.getElementById("foto-perfil");
+
+  fotoPerfil.innerHTML = iconoSeleccionado.outerHTML;
 }
 
 //chats
-function cerrarChat(){
+function cerrarChat() {
   window.location.href = "contactos.html";
 }
 
-function abrirPerfil(){
-  window.location.href = "perfil.html"; 
+function abrirPerfil() {
+  window.location.href = "perfil.html";
 }
