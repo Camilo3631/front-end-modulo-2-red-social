5af770649994873fb83ce1d3a8686decac2f70b3
crislozanor05 <cristina.lozanor05@alumnostrazos.net>
2026-06-08 17:04:57 +0200

    publicaciones usuarios

--- js/contactos.js
+++ js/contactos.js
@@ -104,3 +104,41 @@ export const mostrarUsuarios = (usuariosParaMostrar, contactos) => {
 
 
 
+// FUNCIÓN PARA MOSTRAR LAS PUBLICACIONES
+export function obtenerPublicacionesUsuarios() {
+  const contenedor = document.getElementById("publicaciones-contacto");
+  if (!contenedor) return;
+
+  fetch(`http://localhost:3000/publicaciones/todas`)
+    .then((res) => res.json())
+    .then((respuesta) => {
+      const publicaciones = respuesta.data || [];
+      contenedor.innerHTML = "";
+
+      if (publicaciones.length === 0) {
+        contenedor.innerHTML = `<p style="text-align:center; color:gray; margin-top:20px;">Aún no hay publicaciones.</p>`;
+        return;
+      }
+
+      publicaciones.reverse().forEach((post) => {
+        const divCard = document.createElement("div");
+        divCard.classList.add("publicacion-card");
+
+        divCard.innerHTML = `
+            <div class="publicacion-header">
+                <div class="usuario-info">
+                    <div class="avatar-mini">
+                        <i class="fa-solid fa-user"></i>
+                    </div>
+                    <span class="pub-username">${post.username}</span>
+                </div>
+            </div>
+            <div class="publicacion-contenido">
+                <p>${post.texto}</p>
+            </div>
+        `;
+        contenedor.appendChild(divCard);
+      });
+    })
+    .catch((err) => console.error("Error en el GET de publicaciones:", err));
+}
